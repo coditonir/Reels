dd00e208aa871e4d9f8855706e10bf557dac57ee
Taha <tahadavari1381@gmail.com>
2021-09-14 18:23:04 +0430

    reels1 :: windows10 logo

---
+++ windows10_logo.py
@@ -0,0 +1,31 @@
+# Reels 1
+# Windows10 logo
+# coditon.ir
+
+from turtle import *
+from time import sleep
+speed(1)
+bgcolor('black')
+sleep(3)
+penup()
+goto(-50, 60)
+pendown()
+color('#00adef')
+begin_fill()
+goto(100, 100)
+goto(100, -100)
+goto(-50, -60)
+
+goto(-50, -60)
+end_fill()
+
+goto(15, 100)
+color('black')
+width(10)
+goto(15, -100)
+penup()
+goto(100, 0)
+pendown()
+goto(-100, 0)
+done()
+sleep(5)
